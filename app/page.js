bac90d6af9c051f1cfb916e80e5dbb4084c14df5
surajsinghgore <surajthakurrs45@gmail.com>
2023-11-22 20:03:28 +0530

    bg change

--- app/page.js
+++ app/page.js
@@ -1,28 +1,31 @@
 import Image from "next/legacy/image";
+import Link from "next/link";
+import style from './page.module.css'
 export default function Home() {
   return (
+    <div className={style.homeDiv}>
    <div className="flex pt-20 justify-center  space-x-6">
 
    
 
 {/* Build Api Projects */}
 
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
     <div className="relative w-22 h-52"> 
-        <Image class="rounded-t-lg" src="/api.webp" alt="web" layout='fill'/>
+        <Image className="rounded-t-lg" src="/api.webp" alt="web" layout='fill'/>
     </div>
       
         
     </a>
-    <div class="p-5">
+    <div className="p-5">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">REST API&apos;s PROJECTS</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">REST API&apos;s PROJECTS</h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the Apis List created my me. Which helps to integrate with any projects</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the Apis List created my me. Which helps to integrate with any projects</p>
+        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Explore APIS
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
         </a>
@@ -30,27 +33,29 @@ export default function Home() {
 </div>
 
 {/* Integrate Api Projects */}
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
     <div className="relative w-22 h-52"> 
-        <Image class="rounded-t-lg" src="/udapi.jpeg" alt="web" layout='fill'/>
+        <Image className="rounded-t-lg" src="/udapi.jpeg" alt="web" layout='fill'/>
     </div>
       
         
     </a>
-    <div class="p-5">
+    <div className="p-5">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">INTEGRATED API&apos; s PROJECTS</h5>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">INTEGRATED API&apos; s PROJECTS</h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the Api&lsquo; integrated.It helps to integrate with any third party apis in projects </p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the Api&lsquo; integrated.It helps to integrate with any third party apis in projects </p>
+        <Link href="/IntegratedApi" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           View Projects
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
-        </a>
+        </Link>
     </div>
 </div>
+   </div>
+
    </div>
   )
 }
